61eff667c93ddb405faebe81e8d665165c8c7ad3
120356aa <yurlovandrew@gmail.com>
2020-08-23 22:15:28 -0700

    -f created tags table

---
+++ data/migrations/20200823221248_tags.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("tags", tbl => {
+        tbl.integer("snippet_id")
+            .references("id")
+            .inTable("snippets")
+            .notNullable()
+            .index();
+        tbl.integer("tech_id")
+            .references("id")
+            .inTable("technologies")
+            .notNullable();
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("tags");
+};
