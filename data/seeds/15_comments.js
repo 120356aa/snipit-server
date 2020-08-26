70fcc5a6cace1771880e51686796cc81a0ea368f
120356aa <yurlovandrew@gmail.com>
2020-08-25 22:36:41 -0700

    -f comments seeded

---
+++ data/seeds/15_comments.js
@@ -0,0 +1,57 @@
+let faker = require('faker');
+
+exports.seed = function(knex) {
+  return knex('comments').truncate()
+    .then(function () {
+      return knex('comments').insert([
+        {
+          snippet_id: 1,
+          user_id: 1,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 1,
+          user_id: 2,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 3,
+          user_id: 5,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 4,
+          user_id: 4,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 5,
+          user_id: 4,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 7,
+          user_id: 3,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 9,
+          user_id: 2,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          snippet_id: 8,
+          user_id: 4,
+          comment: `${faker.lorem.sentences()}`,
+          created_at: `${faker.date.future()}`
+        },
+      ]);
+    });
+};
