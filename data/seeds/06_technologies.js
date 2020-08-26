8c0b8d831ef5f1f1688b23eab17add8f98b11b86
120356aa <yurlovandrew@gmail.com>
2020-08-25 21:50:25 -0700

    -f technologies seeded

---
+++ data/seeds/06_technologies.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex) {
+  return knex('technologies').truncate()
+    .then(function () {
+      return knex('technologies').insert([
+        { technology: 'javascript' },
+        { technology: 'php' },
+        { technology: 'python' },
+        { technology: 'html' },
+        { technology: 'css' },
+        { technology: 'react' },
+        { technology: 'vue' },
+        { technology: 'angular' },
+        { technology: 'swift' },
+        { technology: 'node' },
+        { technology: 'go' },
+        { technology: 'sql' }
+      ]);
+    });
+};
