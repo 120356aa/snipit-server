676ca12dce51f4e7f224bdff53e754bdd568add8
120356aa <yurlovandrew@gmail.com>
2020-08-26 16:50:42 -0700

    -f setup routes

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const helmet = require('helmet'); // Extra security
 const morgan = require("morgan"); // Debugging Tool
 const middlewares = require('./middlewares');
 
+const usersRouter = require('./api/users/usersRouter');
+const snippetsRouter = require('./api/snippets/snippetsRouter');
+const technologiesRouter = require('./api/technologies/technologiesRouter');
+const accountTypeRouter = require('./api/accountType/accountTypeRouter');
+const securityClearanceRouter = require('./api/securityClearance/securityClearanceRouter');
+const authorizationRouter = require('./api/authorization/authorizationRouter');
+
 // cors setup
 const corsOptions = {
     origin: APP_URL,
@@ -28,13 +35,17 @@ server.use(
 // Sanity Check
 server.get('/', (req, res) => res.status(200).json("Server running"));
 
-
 // Simple Error Handling
 server.use(middlewares.notFound); // 404 Handler
 server.use(middlewares.errHandler); // Specified Error
 
 // Routes
-
+server.use('/users', usersRouter);
+server.use('/snippets', snippetsRouter);
+server.use('/technologies', technologiesRouter);
+server.use('/account_type', accountTypeRouter);
+server.use('/security_clearance', securityClearanceRouter);
+server.use('/authorization', authorizationRouter);
 
 server.listen(PORT, () => {
     console.log(`Listening on localhost:${PORT}`);
