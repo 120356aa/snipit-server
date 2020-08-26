dd9834d132a84ca3ae8c0f485167582efc851c12
120356aa <yurlovandrew@gmail.com>
2020-08-25 22:09:53 -0700

    -f snippets seeded

---
+++ data/seeds/10_snippets.js
@@ -0,0 +1,81 @@
+let faker = require('faker');
+
+exports.seed = function(knex) {
+  return knex('snippets').truncate()
+    .then(function () {
+      return knex('snippets').insert([
+        {
+          user_id: 1,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 2,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 2,
+          authorization_id: 2,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 3,
+          authorization_id: 2,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 4,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 4,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 5,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 5,
+          authorization_id: 2,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+        {
+          user_id: 5,
+          authorization_id: 1,
+          title: `${faker.lorem.words}`,
+          description: `${faker.lorem.sentences}`,
+          code: `${faker.lorem.paragraph}`,
+          created_at: `${faker.date.future}`
+        },
+      ]);
+    });
+};
