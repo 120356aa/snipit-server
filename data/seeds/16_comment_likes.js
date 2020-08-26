db9b1740adcd488b0e24f0b3e86185198dfb040d
120356aa <yurlovandrew@gmail.com>
2020-08-25 22:40:14 -0700

    -f comment likes seeded

---
+++ data/seeds/16_comment_likes.js
@@ -0,0 +1,51 @@
+let faker = require('faker');
+
+exports.seed = function(knex) {
+  return knex('comment_likes').truncate()
+    .then(function () {
+      return knex('comment_likes').insert([
+        {
+          comment_id: 1,
+          user_id: 1,
+          view_status: 1,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 4,
+          user_id: 1,
+          view_status: 0,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 5,
+          user_id: 2,
+          view_status: 1,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 4,
+          user_id: 3,
+          view_status: 1,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 6,
+          user_id: 4,
+          view_status: 1,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 2,
+          user_id: 4,
+          view_status: 0,
+          created_at: `${faker.date.future()}`
+        },
+        {
+          comment_id: 6,
+          user_id: 5,
+          view_status: 1,
+          created_at: `${faker.date.future()}`
+        },
+      ]);
+    });
+};
